15456698a8fde110c206af58e5a30e5334d4e0b6
Сушков денис <sushkovdv@cbr.ru>
2020-09-04 08:56:26 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { RelayEnvironmentProvider } from "@umk/statistic-client-relay";
+import { System } from "@umk/statistic-client-components";
+import { WS_URL, URL } from "./config";
+
+function App(): JSX.Element {
+  return (
+    <RelayEnvironmentProvider socketUrl={WS_URL} apiUrl={URL}>
+      <React.Suspense fallback="loading...">
+        <System />
+      </React.Suspense>
+    </RelayEnvironmentProvider>
+  )
+}
+
+export default App;
